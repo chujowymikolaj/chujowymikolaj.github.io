207cd69ab4cc9b28a38adba00186e7c3167a0d67
chujowymikolaj <kinskijames@gmail.com>
2024-11-27 14:13:20 +0100

    Update zaloguj.php

--- zaloguj.php
+++ zaloguj.php
@@ -1,28 +1,11 @@
 <?php
 
-	session_start();
+	$login = $_POST['login'];
+	$haslo = $_POST['haslo'];
+	if($login = "kasia"&& $haslo = "qwerty"){
 	
-	if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
-	{
-		header('Location: index.php');
-		exit();
 	}
-
-	require_once "connect.php";
-
-	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
-	
-	if ($polaczenie->connect_errno!=0)
-	{
-		echo "Error: ".$polaczenie->connect_errno;
-	}
-	else
-	{
-		$login = $_POST['login'];
-		$haslo = $_POST['haslo'];
 		
-		$login = htmlentities($login, ENT_QUOTES, "UTF-8");
-		$haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
 	
 		if ($rezultat = @$polaczenie->query(
 		sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
