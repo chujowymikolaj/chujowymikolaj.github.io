9d13236778a98dee53ac6fb3715e8faec56ae35c
chujowymikolaj <kinskijames@gmail.com>
2024-11-27 13:28:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meat charset="utf-8"/>
+    <meat http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Chujowymikolaj</title>
+</head>
+<body>
+    <text>czy dziala</text>
+</body>
+</html>
