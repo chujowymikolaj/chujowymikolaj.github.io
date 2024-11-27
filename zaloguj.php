b8e5a7b975b514999d5a6867334a764f7cc65d32
chujowymikolaj <kinskijames@gmail.com>
2024-11-27 14:15:31 +0100

    Update zaloguj.php

--- zaloguj.php
+++ zaloguj.php
@@ -3,43 +3,8 @@
 	$login = $_POST['login'];
 	$haslo = $_POST['haslo'];
 	if($login = "kasia"&& $haslo = "qwerty"){
-	echo
-	}
-		
-	
-		if ($rezultat = @$polaczenie->query(
-		sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
-		mysqli_real_escape_string($polaczenie,$login),
-		mysqli_real_escape_string($polaczenie,$haslo))))
-		{
-			$ilu_userow = $rezultat->num_rows;
-			if($ilu_userow>0)
-			{
-				$_SESSION['zalogowany'] = true;
-				
-				$wiersz = $rezultat->fetch_assoc();
-				$_SESSION['id'] = $wiersz['id'];
-				$_SESSION['user'] = $wiersz['user'];
-				$_SESSION['drewno'] = $wiersz['drewno'];
-				$_SESSION['kamien'] = $wiersz['kamien'];
-				$_SESSION['zboze'] = $wiersz['zboze'];
-				$_SESSION['email'] = $wiersz['email'];
-				$_SESSION['dnipremium'] = $wiersz['dnipremium'];
-				
-				unset($_SESSION['blad']);
-				$rezultat->free_result();
-				header('Location: gra.php');
-				
-			} else {
-				
-				$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
-				header('Location: index.php');
-				
-			}
-			
-		}
-		
-		$polaczenie->close();
-	}
-	
+	echo "Poprawne logowanie";
+	}else{
+	echo "NiePoprawne logowanie";
+	}	
 ?>
